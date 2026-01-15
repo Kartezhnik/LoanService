b8f71ba5bccdd12a89144780a1c88042568ae6b0
akim1253 <nieakim1253@gmail.com>
2026-01-16 01:13:27 +0300

    feature: add entities

---
+++ Domain/Entities/Loan.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Entities
+{
+    public class Loan : Entity
+    {
+        public Status Status { get; set; }
+        public string Number { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public int TermValue { get; set; }
+        public decimal InterestValue { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset ModifiedAt { get; set; }
+
+        public Loan(
+            Guid id,
+            Status status,
+            string numder,
+            decimal amount,
+            int termValue,
+            decimal interestValue,
+            DateTimeOffset createdAt,
+            DateTimeOffset modifiedAt) : base(id) 
+        {
+            Id = id;
+            Status = status;
+            Number = numder;
+            Amount = amount;
+            TermValue = termValue;
+            InterestValue = interestValue;
+            CreatedAt = createdAt;
+            ModifiedAt = modifiedAt;
+        }
+    }
+
+    public enum Status
+    {
+        Published,
+        Unpublished
+    }
+}

9342617f68088f4dabdd5df8daded28f3a49869d
akim1253 <nieakim1253@gmail.com>
2026-01-16 04:06:24 +0300

    feature: add toggle loan status interactor

---
+++ Application/Interactors/Loans/Commands/ToggleLoanStatus/ToggleLoanCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interactors.Loans.Commands.ToggleLoanStatus
+{
+    public sealed record ToggleLoanCommand
+    {
+        public Guid Id;
+        public ToggleLoanCommand(Guid id) => Id = id;
+    }
+}
---
+++ Application/Interactors/Loans/Commands/ToggleLoanStatus/ToggleLoanCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+
+namespace Application.Interactors.Loans.Commands.ToggleLoanStatus
+{
+    public class ToggleLoanCommandHandler
+    {
+        private readonly IRepository<Loan> _repository;
+        
+        public ToggleLoanCommandHandler(IRepository<Loan> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Guid> Handle(ToggleLoanCommand command, CancellationToken cancellationToken)
+        {
+            IReadOnlyList<Loan> loans = await _repository.GetByFilterAsync(loan => loan.Id == command.Id, cancellationToken);
+
+            Loan loan = loans.First();
+
+            loan.Status = loan.Status == Status.Published ? Status.Unpublished : Status.Published;
+            loan.ModifiedAt = DateTimeOffset.UtcNow;
+            await _repository.Update(loan);
+
+            return loan.Id;
+        }
+    }
+}
